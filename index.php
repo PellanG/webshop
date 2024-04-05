f4d471d3f3f2ad8281e2a668a7cef3e823c11769
PellanG <pellan.gustafsson@hotmail.com>
2024-04-05 08:17:43 +0200

    rough styling of landingpage

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/aa7322f0de.js" crossorigin="anonymous"></script>
+    <script src="/js/main.js"></script>
+</head>
+
+<body>
+    <section class="video-container">
+        <video class="myvideo" src="assets/nature-movie.mp4" type="video/mp4" autoplay muted loop>
+        </video>
+
+        <header class="header-container">
+            <div class="header-container__title">
+                <h1>Plant <i class="fa-brands fa-pagelines"></i> shoppen</h1>
+
+            </div>
+            <nav class="header-container__navigation">
+                <li>Categories</li>
+                <li>Login</li>
+                <i class="fa-solid fa-cart-shopping">
+                    <div class="cart-count-container"><span class="cart-count__content"><span></div>
+                </i>
+            </nav>
+        </header>
+    </section>
+    <main>
+        <form class="global-search__input"><input type="text" placeholder="Search" /></form>
+        <div class="quotes-container">
+            <h2>Shoppa loss hos oss!</h2>
+        </div>
+
+        <section class="product-selection">
+            <div class="product-selection-container">
+                <div class="product-selection-container__img"></div>
+                <h3 class="product-selection-container__title"></h3>
+            </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div><i class="fa-solid fa-envelope"></i> <span>E-mail</span></div>
+        <div><i class="fa-solid fa-phone"></i> <span>Telephone</span></div>
+        <div><i class="fa-brands fa-facebook"></i><span> Facebook</span></div>
+        <div><i class="fa-solid fa-house"></i><span> Address</span></div>
+    </footer>
+
+</body>
+
+</html>
